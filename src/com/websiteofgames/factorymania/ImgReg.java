44bc08b3c7e92de2570362c9480e85d2c3aaa680
Mystery <websiteemail12345678910@gmail.com>
2021-10-24 11:46:02 -0700

    Last commit; quitting working on the project

---
+++ src/com/websiteofgames/factorymania/ImgReg.java
@@ -0,0 +1,14 @@
+package com.websiteofgames.factorymania;
+
+import javax.swing.*;
+
+public class ImgReg {
+    public ImageIcon LauncherBackgroundImage = new ImageIcon("mountain.jpg");
+    ImgReg(){
+
+
+
+
+
+    }
+}
--- src/com/websiteofgames/factorymania/Launcher.java
+++ src/com/websiteofgames/factorymania/Launcher.java
@@ -1,35 +1,47 @@
 package com.websiteofgames.factorymania;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Launcher {
+public class Launcher extends JFrame{
+
     private JPanel panel;
     private JButton startButton;
+    private JLabel FactoryManiaJlabel;
 
-    public Launcher() {
+    private JLabel LauncherBackgroundImageLabel;
+
+    public Launcher(){
+        ImgReg imgReg = new ImgReg();
+
+        setContentPane(panel);
+setTitle("Factory Mania Launcher");
+setSize(800,600);
+setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+setVisible(true);
+FactoryManiaJlabel.setFont(new Font(null,Font.BOLD,20));
+startButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+LauncherBackgroundImageLabel = new JLabel(imgReg.LauncherBackgroundImage);
+
+
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-JOptionPane.showMessageDialog(null, "Hello World!");
+                System.out.println("I'm Listening!");
             }
         });
     }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Factory Mania Launcher");
-frame.setContentPane(new Launcher().panel);
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setSize(800,600);
-frame.setVisible(true);
+Launcher launcher = new Launcher();
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
 
+    private void createUIComponents() {
 
 
 
     }
 }
-
